2aa2093b012dc0d17bb4211f484225b0882d96f2
aginies <aginies@suse.com>
2025-11-17 14:52:11 +0100

    add expand_host_range

--- utils.h
+++ utils.h
@@ -7,5 +7,6 @@
 // Centralized memory allocation function
 void* safe_malloc(size_t size);
 void* safe_realloc(void* ptr, size_t size);
+char** expand_host_range(const char* host_str, int* count);
 
 #endif // UTILS_H
